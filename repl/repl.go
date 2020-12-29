5a4ff8bc99b5feeba4ab52eb5737f15960be2bec
Wesley Porter <wesleyrporter@gmail.com>
2020-12-29 13:29:31 -0700

    Add an integer evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/wrporter/monkey/evaluator"
 	"github.com/wrporter/monkey/lexer"
 	"github.com/wrporter/monkey/parser"
 	"io"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
