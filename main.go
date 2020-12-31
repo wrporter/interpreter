deaae1300eb1daa64fcbfc56c21ba3537e1515e3
Wesley Porter <wesleyrporter@gmail.com>
2020-12-30 20:31:00 -0700

    Add file execution

--- main.go
+++ main.go
@@ -2,12 +2,48 @@ package main
 
 import (
 	"fmt"
+	"github.com/wrporter/monkey/evaluator"
+	"github.com/wrporter/monkey/lexer"
+	"github.com/wrporter/monkey/object"
+	"github.com/wrporter/monkey/parser"
 	"github.com/wrporter/monkey/repl"
+	"io/ioutil"
 	"os"
 	"os/user"
 )
 
 func main() {
+	if len(os.Args) > 1 {
+		executeFile()
+	} else {
+		executeREPL()
+	}
+}
+
+func executeFile() {
+	filename := os.Args[1]
+	code, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Failed to execute file %s due to error: %v\n", filename, err)
+		os.Exit(1)
+	}
+
+	l := lexer.New(string(code))
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		repl.PrintParserErrors(os.Stderr, p.Errors())
+		os.Exit(1)
+	}
+
+	env := object.NewEnvironment()
+	evaluated := evaluator.Eval(program, env)
+
+	fmt.Println(evaluated.Inspect())
+}
+
+func executeREPL() {
 	u, err := user.Current()
 	if err != nil {
 		panic(err)
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Interpreter
+
+This project is a learning experience on what it takes to build an interpreter. I started with the [Writing an Interpreter in Go](https://interpreterbook.com/) book and plan to extend onto the language.
+
+## Usage
+
+### Shell
+
+Run the interpreter in an interactive shell.
+
+```shell
+go run main.go
+```
+
+### Execute File
+
+Execute a file written in the supported language.
+
+```shell
+go run main.go program.monkey
+```
+
+## Documentation
+
+TODO
