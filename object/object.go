83a64159984541fb836c7ee4ce7d5b5a70919383
Wesley Porter <wesleyrporter@gmail.com>
2020-12-31 14:25:59 -0700

    Add builtin len function

--- object/object.go
+++ object/object.go
@@ -20,6 +20,8 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 
 	FUNCTION_OBJ = "FUNCTION"
+
+	BUILTIN_OBJ = "BUILTIN"
 )
 
 type Object interface {
@@ -91,3 +93,12 @@ func (f *Function) Inspect() string {
 
 	return out.String()
 }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
