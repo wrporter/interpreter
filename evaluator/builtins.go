cd829f0b1dab331970ccdd9c000b5128df056306
Wesley Porter <wesleyrporter@gmail.com>
2021-01-01 08:02:45 -0700

    Add printing function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "github.com/wrporter/monkey/object"
+import (
+	"fmt"
+	"github.com/wrporter/monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": {
@@ -99,6 +102,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
